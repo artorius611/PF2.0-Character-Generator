f25095b2567b9d4f86de533cf442185b772ebb03
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-10-31 17:58:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from .idea.pc import ancestry
-from .idea.pc import character_stats
+from Race.Race import Race
+from Race.character_stats import character_stats
 import os
 
-def main():
+#def main():
     # Create base player stat values
-    c1 = character_stats(10,10,10,10,10,10,0,0)
+   # c1 = character_stats(10,10,10,10,10,10,0,0)
 
 #show current Stats
 print('Str:',c1.chrStr, '\nDex:', c1.chrDex,'\nCon:', c1.chrCon,'\nInt:', c1.chrInt,'\nWis:', c1.chrWis,'\nCha:', c1.chrCha,'\nSpeed:', c1.chrSpd,'\nHP:', c1.chrHP)
