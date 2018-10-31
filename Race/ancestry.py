ebcfd800e0c182810f7149e217af37b1cd5774ed
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-10-31 16:11:09 -0500

    Add files via upload

---
+++ Race/ancestry.py
@@ -0,0 +1,29 @@
+class Race():
+    #Halfling Race
+    def __init__(halfling):
+        # base stats
+        c1.chrDex += 2
+        c1.chrWis += 2
+        c1.chrStr -= 2
+        c1.chrSpd += 25
+        c1.chrHP += 6
+        #choosing the free stat
+        bonus = input('What is your bonus stat?')
+        type(bonus)
+        if bonus == 'Strength':
+            c1.chrStr += 2
+        if bonus == 'Constitution':
+            c1.chrCon += 2
+        if bonus == 'Intelligence':
+            c1.chrInt += 2
+        if bonus == 'Charisma':
+            c1.chrCha += 2
+        else:
+            print('you cant use a duplicate value. Please choose again')
+            c1.chrDex -= 2
+            c1.chrWis -= 2
+            c1.chrStr += 2
+            c1.chrSpd -= 25
+            c1.chrHP -= 6
+            ancestry()
+
---
+++ Race/character_stats.py
@@ -0,0 +1,12 @@
+class character_stats:
+    def __init__(stat, baseStr, baseDex, baseCon, baseInt, baseWis, baseCha, baseSpd,baseHP):
+        stat.chrStr=baseStr
+        stat.chrDex=baseDex
+        stat.chrCon=baseCon
+        stat.chrInt=baseInt
+        stat.chrWis=baseWis
+        stat.chrCha=baseCha
+        stat.chrSpd=baseSpd
+        stat.chrHP=baseHP
+
+    c1 = character_stats(10,10,10,10,10,10,0,0)
