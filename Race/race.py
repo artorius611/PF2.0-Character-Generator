da292e52df4eba94ef00b1100da7c76d90fdc99e
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-11-05 14:04:53 -0600

    Update race.py

--- Race/race.py
+++ Race/race.py
@@ -4,11 +4,11 @@ class Race:
     def __init__(self, name='default'):
         self.name = name
     
-    attributes = {
-        STR : stat(),
-        DEX : stat(),
-        CHA : stat(),
-        INT : stat(),
-        WIS : stat(),
-        CHA : stat(),
+    self.attributes = {
+        'STR' : stat(),
+        'DEX' : stat(),
+        'CHA' : stat(),
+        'INT' : stat(),
+        'WIS' : stat(),
+        'CHA' : stat(),
     }
