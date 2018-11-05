330b84ad7d3fc798681b8aa8bccd6d81a5462381
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-11-05 15:51:14 -0600

    Update race.py

--- Race/race.py
+++ Race/race.py
@@ -12,10 +12,19 @@ class Race:
         'WIS' : stat(),
         'CHA' : stat(),
     }
-    def getMod():
-        for self.attributes['STR']:
-            if 'STR' == 10 or 'STR' ==11:
-                self.mod = 0
-            elif 'STR' ==12 or 'STR' ==13:
-                self.mod += 1
-            
+    def getMod(self,attr):
+        score = self.attributes[attr].score
+        if score>=20:
+            return 5
+        if score>=18:
+            return 4
+        if score>=16:
+            return 3
+        if score>=14:
+            return 2
+        if score>=12:
+            return 1
+        if score>=10:
+            return 0
+        if score>=8:
+            return -1
