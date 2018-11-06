cc2f04b1390fa263b8269d033a3ef4ce29fc64fb
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-11-06 09:29:01 -0600

    Update race.py

--- Race/race.py
+++ Race/race.py
@@ -14,17 +14,4 @@ class Race:
     }
     def getMod(self,attr):
         score = self.attributes[attr].score
-        if score>=20:
-            return 5
-        elif score>=18:
-            return 4
-        elif score>=16:
-            return 3
-        elif score>=14:
-            return 2
-        elif score>=12:
-            return 1
-        elif score>=10:
-            return 0
-        elif score>=8:
-            return -1
+        mod = int((score - 10)/ 2)
