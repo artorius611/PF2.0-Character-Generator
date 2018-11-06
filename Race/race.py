7bdcfa6743d830ea19cdc738eb5d644417e7806d
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-11-06 08:21:00 -0600

    Update race.py

--- Race/race.py
+++ Race/race.py
@@ -16,15 +16,15 @@ class Race:
         score = self.attributes[attr].score
         if score>=20:
             return 5
-        if score>=18:
+        elif score>=18:
             return 4
-        if score>=16:
+        elif score>=16:
             return 3
-        if score>=14:
+        elif score>=14:
             return 2
-        if score>=12:
+        elif score>=12:
             return 1
-        if score>=10:
+        elif score>=10:
             return 0
-        if score>=8:
+        elif score>=8:
             return -1
