cc03c25c90f5e6a004083b7a3cb3c35d4e237d99
alexander weiss <42820958+artorius611@users.noreply.github.com>
2018-11-07 21:44:14 -0600

    Update Race.py

--- Race/Race.py
+++ Race/Race.py
@@ -1,38 +1,36 @@
-from ..stat import stat
+from ..Chr_stat import chr_stat
 
 class Race():
-    def __init__(halfling):
-        #getting the stats from chr_stat
-        halfling_str = score.getMod('STR')
-        halfling_dex = score.getMod('DEX')
-        halfling_con = score.getMod('CON')
-        halfling_int = score.getMod('INT')
-        halfling_wis = score.getMod('WIS')
-        halfling_cha = score.getMod('CHA')
-        halfling_hp =  score.getMod('HP')
-        halfling_spd = score.getMod('SPD')       
-        
+    def __init__(self, ):
+        self.race.stats = Chr_stat()
+        str_score = race.stats.attributes['STR'].score
+        str_mod = race.stats.getMod('STR')
+        dex_score = race.stats.attributes['DEX'].score
+        dex_mod = race.stats.getMod('DEX')
+        con_score = race.stats.attributes['CON'].score
+        con_mod = race.stats.getMod('CON')
+        int_score = race.stats.attributes['INT'].score
+        int_mod = race.stats.getMod('INT')
+        wis_score = race.stats.attributes['WIS'].score
+        wis_mod = race.stats.getMod('WIS')
+        cha_score = race.stats.attributes['CHA'].score
+        cha_mod = race.stats.getMod('CHA')
+
+    def getScore(self,attribute):
+        return race.stats.attributes[attribute]
+
+class Halfling():
+       
         # modifiying the stats
         halfling_dex += 2
         halfling_wis += 2
         halfling_str -= 2
         halfling_spd += 25
         halfling_hp += 6
-        bonus = input('What is your bonus stat?')
-        type(bonus)
-        if bonus == 'Strength':
-            halfling_str += 2
-        elif bonus == 'Constitution':
-            halfling_con += 2
-        elif bonus == 'Intelligence':
-            halfling_int += 2
-        elif bonus == 'Charisma':
-            halfling_cha += 2
-        else:
-            print('you cant use a duplicate value. Please choose again')
-            halfling_dex -= 2
-            halfling_wis -= 2
-            halfling_str += 2
-            halfling_spd -= 25
-            halfling_hp -= 6
-            
+        bonus_stat = input('What is your bonus stat?')
+        type(bonus_stat)
+        def applyBonus(self, bonus_stat):
+            if bonus_stat == 'INT' or 'STR' or 'CON' or 'CHA':
+                race.stats.attributes[bonus_stat] += 2
+            else:
+                print ('You cant use a duplicate value.')
